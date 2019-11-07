6baa9b5dafa42ca3c3e5ca860921cff0344661b1
mhuang <mhuang_email@163.com>
2019-11-07 09:21:53 +0800

    1.3.0

--- src/main/java/tech/mhuang/ext/spring/pool/SpringThreadPool.java
+++ src/main/java/tech/mhuang/ext/spring/pool/SpringThreadPool.java
@@ -2,14 +2,34 @@ package tech.mhuang.ext.spring.pool;
 
 import tech.mhuang.core.pool.BaseExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import tech.mhuang.core.pool.DefaultThreadPool;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  *
- * spring 线程池、供kafka使用
+ * spring 线程池
  *
  * @author mhuang
  * @since 1.0.0
  */
 public class SpringThreadPool extends ThreadPoolTaskExecutor implements BaseExecutor {
 
+    /**
+     * default threadpool queue size
+     */
+    private final static Integer DEFAULT_QUEUE_CAPACITY = 20;
+
+    /**
+     * default threadpool keepalive secord
+     */
+    private final static Integer DEFAULT_KEEPALIVE_SECOND = 200;
+
+    public SpringThreadPool(){
+        this.setCorePoolSize(DefaultThreadPool.DEFAULT_CORE_POOL_SIZE);
+        this.setMaxPoolSize(DefaultThreadPool.DEFAULT_MAX_POOL_SIZE);
+        this.setQueueCapacity(DEFAULT_QUEUE_CAPACITY);
+        this.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        this.setKeepAliveSeconds(DEFAULT_KEEPALIVE_SECOND);
+    }
 }
